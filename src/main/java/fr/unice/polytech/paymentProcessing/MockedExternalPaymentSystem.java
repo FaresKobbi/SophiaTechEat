379343a3d97df6529031b9101b8c057340a6cfa3
FaresKobbi <fares.kobbi@gmail.com>
2025-10-08 11:17:26 +0200

    PaymentProcessor, PaymentService and Order implemented but not tested #9

---
+++ src/main/java/fr/unice/polytech/paymentProcessing/MockedExternalPaymentSystem.java
@@ -0,0 +1,4 @@
+package fr.unice.polytech.paymentProcessing;
+
+public class MockedExternalPaymentSystem {
+}
---
+++ src/test/java/fr/unice/polytech/paymentProcessing/PaymentProcessorTest.java
@@ -0,0 +1,12 @@
+package fr.unice.polytech.paymentProcessing;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentProcessorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
