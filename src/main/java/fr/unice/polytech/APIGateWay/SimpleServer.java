669c45725a972fbae32572e55f66e0c22660cbb3
youssefBM2104 <mzoughia2104@gmail.com>
2025-11-11 23:43:11 +0100

    apigateway completed

---
+++ src/main/java/fr/unice/polytech/APIGateWay/SimpleServer.java
@@ -0,0 +1,42 @@
+package fr.unice.polytech.APIGateWay;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+class SimpleServer {
+    private final HttpServer server;
+    private final int port;
+
+    public SimpleServer(int port) throws IOException {
+        this.port = port;
+        this.server = HttpServer.create(new InetSocketAddress(port), 0);
+        this.server.setExecutor(Executors.newCachedThreadPool());
+    }
+
+    public void start(ApiRegistry registry) {
+        server.createContext("/", exchange -> {
+            try {
+                registry.handle(exchange);
+            } catch (Exception e) {
+                System.err.println("Error handling exchange: " + e.getMessage());
+                e.printStackTrace();
+                sendResponse(exchange, 500, "{\"error\":\"Internal Server Error: " + e.getMessage() + "\"}");
+            }
+        });
+        server.start();
+    }
+
+    private void sendResponse(HttpExchange exchange, int statusCode, String response) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        byte[] responseBytes = response.getBytes("UTF-8");
+        exchange.sendResponseHeaders(statusCode, responseBytes.length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(responseBytes);
+        }
+    }
+}
